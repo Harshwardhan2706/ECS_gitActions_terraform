d6c25f84809df9f60a6f9b1056549f4a0a17f2cf
Harshwardhan2706 <100345385+Harshwardhan2706@users.noreply.github.com>
2024-07-21 17:46:35 +0530

    Update config.tf

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = ${{secrets.AWS_SECRET_ACCESS_KEY}}
-  secret_key_id = ${{secrets.AWS_SECRET_ACCESS_KEY_ID}} 
+  access_key = ${{secrets.AWS_SECRET_ACCESS_KEY_ID}}
+  secret_key = ${{secrets.AWS_SECRET_ACCESS_KEY}} 
 }
