322d6160086156849859d23b8895e62814d63dec
Harshwardhan2706 <100345385+Harshwardhan2706@users.noreply.github.com>
2024-07-21 20:21:46 +0530

    Update ecs.tf

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -1,18 +1,22 @@
+# Define the ECS cluster
 resource "aws_ecs_cluster" "ecs" {
   name = "app_cluster"
 }
 
+# Define the ECS service
 resource "aws_ecs_service" "service" {
-  name = "app_service"
+  name                   = "app_service"
   cluster                = aws_ecs_cluster.ecs.arn
-  launch_type            = "FARGATE"
+  launch_type             = "FARGATE"
   enable_execute_command = true
 
-  deployment_maximum_percent         = 200
+  # Deployment configuration
+  deployment_maximum_percent   = 200
   deployment_minimum_healthy_percent = 100
-  desired_count                      = 1
-  task_definition                    = aws_ecs_task_definition.td.arn
+  desired_count                 = 1
+  task_definition              = aws_ecs_task_definition.td.arn
 
+  # Network configuration
   network_configuration {
     assign_public_ip = true
     security_groups  = [aws_security_group.sg.id]
@@ -20,28 +24,32 @@ resource "aws_ecs_service" "service" {
   }
 }
 
+# Define the ECS task definition
 resource "aws_ecs_task_definition" "td" {
+  # Container definitions (consider separating container definition into a separate resource)
   container_definitions = jsonencode([
     {
-      name         = "app"
-      image        = "***.dkr.ecr.us-east-2.amazonaws.com/app_repo"
-      cpu          = 256
-      memory       = 512
-      essential    = true
+      name        = "app"
+      image       = "***.dkr.ecr.us-east-2.amazonaws.com/app_repo"
+      cpu         = 256
+      memory      = 512
+      essential   = true
       portMappings = [
         {
           containerPort = 80
           hostPort      = 80
-        }
-      ]
-    }
+        },
+      ],
+    },
   ])
-  family                   = "app"
+
+  family                  = "app"
   requires_compatibilities = ["FARGATE"]
 
-  cpu                = "256"
-  memory             = "512"
-  network_mode       = "awsvpc"
-  task_role_arn      = "arn:aws:iam::***:role/ecsTaskExecutionRole"
+  # Task execution resources
+  cpu               = "256"
+  memory            = "512"
+  network_mode        = "awsvpc"
+  task_role_arn       = "arn:aws:iam::***:role/ecsTaskExecutionRole"
   execution_role_arn = "arn:aws:iam::***:role/ecsTaskExecutionRole"
 }
