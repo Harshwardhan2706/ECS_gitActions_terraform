a575a47c03cca4a8b76a1dda8e9f0735d1f277d8
Harshwardhan2706 <100345385+Harshwardhan2706@users.noreply.github.com>
2024-07-21 20:14:24 +0530

    Update config.tf

--- infra/config.tf
+++ infra/config.tf
@@ -1,5 +1,5 @@
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = "AKIAZI2LFLAWVVRMURWE"
-  secret_key = "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" 
-}
+##provider "aws" {
+##  region     = "ap-south-1"
+##  access_key = "AKIAZI2LFLAWVVRMURWE"
+##  secret_key = "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" 
+##}
